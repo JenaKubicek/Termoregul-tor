cce2208dcffdf14f67aa2095977f9d2f00d0d467
JenaKubicek <jena.kubicek@gmail.com>
2022-01-04 15:57:40 +0100

    Ukazuje teplotu na LCD

--- src/main.c
+++ src/main.c
@@ -1,49 +1,58 @@
 #include "stm8s.h"
 #include "milis.h"
-
-/*#include "delay.h"*/
-/*#include <stdio.h>*/
-/*#include "../lib/uart.c"*/
-
+#include "stm8_hd44780.h"
+#include "spse_stm8.h"
+#include "delay.h"
+#include <stdio.h>
 #define _ISOC99_SOURCE
 #define _GNU_SOURCE
 
-#define LED_PORT GPIOC
-#define LED_PIN  GPIO_PIN_5
-#define LED_HIGH   GPIO_WriteHigh(LED_PORT, LED_PIN)
-#define LED_LOW  GPIO_WriteLow(LED_PORT, LED_PIN)
-#define LED_TOGG GPIO_WriteReverse(LED_PORT, LED_PIN)
+void ADC_init(void){
+// na pinech/vstupech ADC_IN2 (PB2) a ADC_IN3 (PB3) vypneme vstupní buffer
+ADC2_SchmittTriggerConfig(ADC2_SCHMITTTRIG_CHANNEL2,DISABLE);
+// nastavíme clock pro ADC (16MHz / 4 = 4MHz)
+ADC2_PrescalerConfig(ADC2_PRESSEL_FCPU_D4);
+// volíme zarovnání výsledku (typicky vpravo, jen vyjmečně je výhodné vlevo)
+ADC2_AlignConfig(ADC2_ALIGN_RIGHT);
+// nasatvíme multiplexer na některý ze vstupních kanálů
+ADC2_Select_Channel(ADC2_CHANNEL_2);
+// rozběhneme AD převodník
+ADC2_Cmd(ENABLE);
+// počkáme než se AD převodník rozběhne (~7us)
+ADC2_Startup_Wait();
+}
 
-#define BTN_PORT GPIOE
-#define BTN_PIN  GPIO_PIN_4
-#define BTN_PUSH (GPIO_ReadInputPin(BTN_PORT, BTN_PIN)==RESET) 
-
-
-void setup(void)
+void init(void)
 {
     CLK_HSIPrescalerConfig(CLK_PRESCALER_HSIDIV1);      // taktovani MCU na 16MHz
-    GPIO_Init(LED_PORT, LED_PIN, GPIO_MODE_OUT_PP_LOW_SLOW);
     init_milis();
+    lcd_init();
+    ADC_init();
 }
 
 
 int main(void)
 {
-    uint32_t time = 0;
-
-    setup();
-    /*init_uart();*/
+    init();
+    uint32_t timeA = 0;
+    uint16_t adc_value;
+    char text[32];
+    int16_t voltage1;
+    int16_t teplota1;
+    int16_t teplota2;
 
     while (1) {
+        if (milis()-timeA>100){
+            timeA=milis();
+            adc_value = ADC_get(ADC2_CHANNEL_2); // do adc_value ulož výsledek převodu vstupu ADC_IN2 (PB2)
+            voltage1 = ((uint32_t)adc_value*5000 + 512)/1024;
+            teplota1=voltage1/10;
+            teplota2=voltage1%10;
 
-        if (milis() - time > 333 && BTN_PUSH) {
-            LED_TOGG; 
-            time = milis();
         }
-
-        /*LED_FLIP; */
-        /*_delay_ms(333);*/
-        /*printf("Funguje to!!!\n");*/
+        lcd_gotoxy(0, 0);
+        sprintf(text,"Teplota = %2u %1u C",teplota1,teplota2);
+        lcd_puts(text);
     }
 }
 
